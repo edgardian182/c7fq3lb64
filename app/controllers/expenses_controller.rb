30bdea339608c83a9805790c27e281cb74b596ff
Edgar Quintana <edgarquintana984@hotmail.com>
2016-06-15 17:58:30 -0500

    Funcionalidad busqueda agregada

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,12 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+    if params[:concept] != ""
+      # Este código hace una busqueda no exacta
+      @expenses = Expense.where("lower(concept) LIKE ?", "%#{params[:concept]}%")
+    else
+      @expenses = Expense.order("date DESC")
+    end
+
   end
 end
